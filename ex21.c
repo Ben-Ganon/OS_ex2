5772fa3ffc60592de9ba731023874cd14a1d145c
ben <ganonben@gmail.com>
2022-05-06 13:07:56 +0300

    rough draft of ex21 main function

--- ex21.c
+++ ex21.c
@@ -7,17 +7,74 @@
 #include "sys/types.h"
 #include "dirent.h"
 #include "errno.h"
+#include "math.h"
 
-int main(int argc, char* argv[]) {
-    char* path1 = argv[1];
-    char* path2 = argv[2];
+#define SIZE 100
+
+int main(int argc, char *argv[]) {
+    char *path1 = argv[1];
+    char *path2 = argv[2];
+    char buff1[SIZE] = {};
+    char buff2[SIZE] = {};
+    int isCapDiff = 0;
     int fd1 = open(path1, O_RDONLY);
+    if (fd1 < 0) {
+        perror("Error in ex21");
+        exit(-1);
+    }
     int fd2 = open(path2, O_RDONLY);
-    if(fd1 < 0 || fd2 < 0) {
-        perror("error opening files");
+    if (fd2 < 0) {
+        perror("Error in ex21");
+        exit(-1);
+    }
+
+    int readNum1 = read(fd1, buff1, SIZE);
+    int readNum2 = read(fd2, buff2, SIZE);
+    if(readNum2 < 0 || readNum1 < 0) {
         close(fd1);
         close(fd2);
-        exit(1);
+        perror("Error in ex21");
+        exit(-1);
+    }
+    char *endBuff1 = &buff1[readNum1];
+    char *endBuff2 = &buff2[readNum2];
+    char *p1 = buff1;
+    char *p2 = buff2;
+
+    while (1) {
+        if(readNum1 == 0 && readNum2 == 0) {
+            break;
+        }
+        while ((*p1 == ' ' || *p1 == '\n') && p1 != endBuff1 && readNum1) {
+            p1++;
+        }
+        while ((*p2 == ' ' || *p2 == '\n') && p2 != endBuff2 && readNum2) {
+            p2++;
+        }
+        if(p1 == endBuff1) {
+            readNum1 = read(fd1, buff1, SIZE);
+            continue;
+        }
+        if(p2 == endBuff2) {
+            readNum2 = read(fd2, buff2, SIZE);
+            continue;
+        }
+        if (*p1 == *p2) {
+            p1++;
+            p2++;
+            continue;
+        }
+        if (abs(*p2 - *p1) != 32) {
+            setenv("?", "2", 1);
+            return 2;
+        }
+        if (abs(*p2 - *p1) == 32) {
+            isCapDiff = 1;
+            p1++;
+            p2++;
+            continue;
+        }
+
     }
 
 }
