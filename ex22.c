872c600ebf572d537545200aced6e23c068c8a08
ben <ganonben@gmail.com>
2022-05-10 14:31:52 +0300

    continue work

--- ex22.c
+++ ex22.c
@@ -22,29 +22,34 @@ char *getFileType(char *input, int back) {
     return ptr;
 }
 
-int findOutFile(char* dirPath) {
-    DIR* curr = opendir(dirPath);
+int findOutFile(char *dirPath) {
+    DIR *curr = opendir(dirPath);
     if (curr == NULL) {
         perror("Not a valid directory");
         exit(-1);
     }
     struct dirent *currEntry;
     while ((currEntry = readdir(curr)) != NULL) {
-        if(strcmp(getFileType(currEntry->d_name, 4), ".out") == 0)
+        if (strcmp(getFileType(currEntry->d_name, 4), ".out") == 0)
             return 1;
     }
     closedir(curr);
     return 0;
 }
 
-int runStudent(char *root, char *dirPath, int inputFd, int outputFd) {
+int runStudent(char *root, char *dirPath, int inputFd, int errorFd, char*outPutComp, char* compDir) {
     int stat;
     char tempPath[150];
     strcpy(tempPath, root);
     strcat(tempPath, "/");
     char *stuPath = strcat(tempPath, dirPath);
     DIR *stuDir = opendir(stuPath);
-    int errorcode = 0;
+    int exitCode = 0;
+    int oldIn = dup(0);
+    int oldOut = dup(1);
+    int save_err = dup(2);
+
+
     if (stuDir == NULL) {
         perror("Not a valid directory");
         exit(-1);
@@ -59,37 +64,67 @@ int runStudent(char *root, char *dirPath, int inputFd, int outputFd) {
             chdir(stuPath);
             char tempCwd[LINE_LEN];
             getcwd(tempCwd, LINE_LEN);
-            errorcode++;
-            int save_err = dup(2);
-            int gccOut = open("gccOut.txt", O_CREAT  | S_IWUSR | S_IRUSR, 0666);
-            dup2(gccOut, 2);
+            exitCode++;
+            dup2(errorFd, 2);
             char *args[] = {"gcc", "-w", currEntry->d_name, "-o a.out", NULL};
             pid_t pid = fork();
             if (pid == 0) {
                 int retCode = execvp(args[0], args);
                 if (retCode < 0) {
-                    perror("Error execvp");
+                    perror("Error execvp1");
                     exit(-1);
                 }
             } else {
                 wait(&stat);
-                char errOut[LINE_LEN] = {};
-                int readErr = read(gccOut, errOut, LINE_LEN);
-                if (readErr < 0) {
-                    perror("Error in read");
-                    close(gccOut);
-                    closedir(stuDir);
-                    exit(-1);
-                } else if (findOutFile(tempCwd))
-                    errorcode++;
+                if(stat == 0) {
+                    exitCode++;
+                    int outputFd = open("out.txt", O_CREAT| S_IWUSR, 0666);
+                    dup2(inputFd, 0);
+                    dup2(outputFd, 1);
+                    char *argRunStudent[] = {"./a.out", NULL};
+                    pid = fork();
+                    if(pid == 0) {
+                        int retCode = execvp(argRunStudent[0], argRunStudent);
+                        if (retCode < 0) {
+                            perror("Error execvp2");
+                            exit(-1);
+                        }
+                    } else {
+                        wait(&stat);
+                        close(outputFd);
+                        char* argRunComp[] = {compDir, outPutComp, "out.txt", NULL};
+                        pid = fork();
+                        if(pid ==0) {
+                            int retCode = execvp(argRunComp[0], argRunComp);
+                            if (retCode < 0) {
+                                perror("Error execvp3");
+                                exit(-1);
+                            }
+                        } else {
+                            wait(&stat);
+                            remove("out.txt");
+                            if(stat == 1) {
+                                exitCode += 3;
+                            } else if (stat == 2) {
+                                exitCode++;
+                            }else if (stat ==3) {
+                                exitCode += 2;
+                            } else {
+                                continue;
+                            }
+                        }
+                    }
+
+                }
 
             }
-            close(gccOut);
-            remove("gccOut.txt");
         }
     }
+    dup2(oldIn, 0);
+    dup2(oldOut, 1);
+    dup2(save_err, 2);
     closedir(stuDir);
-    return errorcode;
+    return exitCode;
 }
 
 
@@ -110,7 +145,6 @@ int main(int argc, char *argv[]) {
         close(fdConfig);
         exit(-1);
     }
-    int rootNum, inputNum, outputNum = 0;
     root = strtok(fileRead, "\n");
     input = strtok(NULL, "\n");
     output = strtok(NULL, "\n");
@@ -121,6 +155,10 @@ int main(int argc, char *argv[]) {
         exit(-1);
     }
 
+    char compDir[LINE_LEN];
+    getcwd(compDir, LINE_LEN);
+    strcat(compDir, "/comp.out");
+
     struct dirent *currStd;
     int inputFd = open(input, O_RDONLY);
     int outputFd = open(output, O_RDONLY);
@@ -130,11 +168,14 @@ int main(int argc, char *argv[]) {
         close(fdConfig);
         exit(-1);
     }
+    chdir(root);
+    int errorFd = open("errors.txt", O_CREAT| S_IWUSR, 0666);
     while ((currStd = readdir(rootDir)) != NULL) {
-        if (currStd->d_type != DT_DIR)
+        if (currStd->d_type != DT_DIR || currStd->d_name[0] =='.')
             continue;
-        int status = runStudent(root, currStd->d_name, inputFd, outputFd);
+        int status = runStudent(root, currStd->d_name, inputFd, errorFd, output, compDir);
         printf("%d, %s\n", status, currStd->d_name);
     }
+    close(errorFd);
     close(fdConfig);
 }
